b0ed9aae1764cfdfc6c26db03cdb6fe954874be1
sahilsol0 <sahilsolodev@gmail.com>
2024-11-01 10:31:30 +0530

    added templates, urls and views for register and login

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import login
+from .views import success, user_login, user_register
 
 urlpatterns = [  
-    path('login/', login.as_view(), name='home'),
+    path('login/', user_login.as_view(), name='user_login'),
+    path('register/', user_register.as_view(), name='user_register'),
+    path('success/', success.as_view(), name='register_success'),
 ]
 
--- authentication/views.py
+++ authentication/views.py
@@ -2,6 +2,18 @@ from django.shortcuts import render
 from django.contrib.auth.models import User, auth
 from django.views import View
 
-class login(View):
+class user_login(View):
     def get(self, request):
         return render(request, 'login.html',context={})
+    
+class user_register(View):
+    def get(self, request):
+        return render(request, 'register.html',context={})
+    
+class user_logout(View):
+    def get(self, request):
+        pass
+    
+class success(View):
+    def get(self, request):
+        return render(request, 'register_success.html',context={})
